6975ef0f7eada62b0c41d43eb3d679c6664a28f0
Jessica <sea.jessicac@gmail.com>
2018-08-19 00:33:43 -0400

    new quiz who dis all javascript done

--- scripts.js
+++ scripts.js
@@ -1,63 +1,68 @@
 const questions = [
   { qOneA: 1, 
-    qOneB: 2, 
-    qOneC: 3, 
-    qTwoA: 3,
-    qTwoB: 2,
+    qOneB: 1, 
+    qOneC: 5, 
+    qOneD: 1,
+    qTwoA: 1,
+    qTwoB: 5,
     qTwoC: 1,
+    qTwoD: 1,
     qThreeA: 1,
-    qThreeB: 3,
-    qThreeC: 2,
-    qFourA: 2,
-    qFourB: 3,
+    qThreeB: 1,
+    qThreeC: 1,
+    qThreeD: 5,
+    qFourA: 1,
+    qFourB: 5,
     qFourC: 1,
-    qFiveA: 3,
-    qFiveB: 2,
-    qFiveC: 1,
+    qFourD: 1,
+    qFiveA: 1,
+    qFiveB: 1,
+    qFiveC: 5,
+    qFiveD: 1,
+    qSixA: 1,
+    qSixB: 1,
+    qSixC: 1,
+    qSixD: 5,
+    qSevenA: 5,
+    qSevenB: 1,
+    qSevenC: 1,
+    qSevenD: 1,
+    qEightA: 1,
+    qEightB: 1,
+    qEightC: 5,
+    qEightD: 1,
   }
 ];
 // quotes.quoteA.quote1.saying;
-const quotes = {
-  quoteA: [
+const scores = {
+  low: [ 
     quote1 = {
-      saying: `"That is so fetch!"`,
-      imgUrl: 'assets/a1.gif'
+      saying: `OMG You need to watch Mean Girls again!`,
+      imgUrl: 'assets/youcant.gif'
     },
     quote2 = {
-      saying: `"I'm not a regular mom, I'm a cool mom.”`,
-      imgUrl: 'assets/a2.gif'
+      saying: `You are not a fan..`,
+      imgUrl: 'assets/doesnt.gif'
     },
-    quote3 = {
-      saying: `"Get in loser, we're going shopping."`,
+  ],
+  moderate: [
+    quote1 = {
+      saying: `Not bad but get in the car! We're watching the movie 5 more times..`,
       imgUrl: 'assets/a3.gif'
     },
-  ],
-  quoteB: [
-    quote1 = {
-      saying: `"Four for you Glen Coco, you go Glen Coco! And none for Gretchen Weiners. Bye."`,
-      imgUrl: 'assets/b1.gif',
-    },
     quote2 = {
-      saying: `“Don’t have sex. Because you will get pregnant, and die."`,
-      imgUrl: 'assets/b2.gif',
-    },
-    quote3 = {
-      saying: `“But you’re, like, really pretty… So you agree? You think you’re really pretty?"`,
-      imgUrl: 'assets/b3.gif',
+      saying: `Not bad... but watch the movie again and you can try again.`,
+      imgUrl: 'assets/try.gif',
     },
   ],
-  quoteC: [
+  high: [
     quote1 = {
-      saying: `“Gretchen, I’m sorry I laughed at you that time you got diarrhea at Barnes & Noble. And I’m sorry for telling everyone about it. And I’m sorry for repeating it now.”`,
-      imgUrl: 'assets/c1.gif',
+      saying: `CONGRATS!! You're a super fan!`,
+      imgUrl: 'assets/taco.gif',
     },
     quote2 = {
-      saying: `"I saw Cady Heron wearing army pants and flip flops, so I bought army pants and flip flops."`,
-      imgUrl: 'assets/c2.gif',
-    },
-    quote3 = {
-      saying: `“I'm sorry that people are so jealous of me... but I can't help it that I'm so popular.”`,
-      imgUrl: 'assets/c3.gif',
+      saying: `CONGRATS!! You CAN sit with us!`,
+      imgUrl: 'assets/lunch.gif',
     },
   ],
 }
@@ -75,34 +80,95 @@ $(function(){
     const userQFourChoice = $('input[name=qFour]:checked').data('point');
     
     const userQFiveChoice = $('input[name=qFive]:checked').data('point');
+
+    const userQSixChoice = $('input[name=qSix]:checked').data('point');
+
+    const userQSevenChoice = $('input[name=qSeven]:checked').data('point');
+
+    const userQEightChoice = $('input[name=qEight]:checked').data('point');
     
-    console.log(userQOneChoice, userQTwoChoice, userQThreeChoice, userQFourChoice, userQFiveChoice);
+    console.log(userQOneChoice, userQTwoChoice, userQThreeChoice, userQFourChoice, userQFiveChoice, userQSixChoice, userQSevenChoice, userQEightChoice);
     
-    let userChoiceTotal = userQOneChoice + userQTwoChoice + userQThreeChoice + userQFourChoice + userQFiveChoice;
+    let userChoiceTotal = userQOneChoice + userQTwoChoice + userQThreeChoice + userQFourChoice + userQFiveChoice + userQSixChoice + userQSevenChoice + userQEightChoice;
     
     console.log(userChoiceTotal);
       
     let result;
-      if(userChoiceTotal <= 7) {
-        result = quotes.quoteA;
+      if(userChoiceTotal <= 20) {
+        result = scores.low;
         console.log(result);
-      } else if (userChoiceTotal >=8 && userChoiceTotal <=12) {
-        result = quotes.quoteB;
+      } else if (userChoiceTotal >=21 && userChoiceTotal <=33) {
+        result = scores.moderate;
           console.log(result);
         } else {
-          (userChoiceTotal >= 15);
-          result = quotes.quoteC;
+          (userChoiceTotal >= 36);
+          result = scores.high;
           console.log(result);
         }
       
-      let randomQuote = result[Math.floor(Math.random()*result.length)];
+      let randomResults = result[Math.floor(Math.random()*result.length)];
 
-      console.log(randomQuote.saying, randomQuote.imgUrl);
+      console.log(randomResults.saying, randomResults.imgUrl);
 
-      $('.yourQuote').html(`<h2 class="quoteAnswer">${randomQuote.saying}</h2>`);
-      const image = $('<img>').attr('src', randomQuote.imgUrl);
+      $('.yourQuote').html(`<h2 class="quoteAnswer">${randomResults.saying}</h2>`);
+      const image = $('<img>').attr('src', randomResults.imgUrl);
       $('.yourImage').html(image);
 
+      let answerOne;
+        if(userQOneChoice === 1) {
+          $('.answerOneResult').html(`<h2>Omg Wrong!!</h2> <img src="assets/shocked.gif">`);
+        } else {
+          $('.answerOneResult').html(`<h2>YOU'RE CORRECT!</h2> <img src="assets/b1.gif">`);
+        };
+      
+      let answerTwo;
+        if(userQTwoChoice === 1) {
+          $('.answerTwoResult').html(`<h2>Omg Wrong!!</h2> <img src="assets/shocked2.gif">`);
+        } else {
+          $('.answerTwoResult').html(`<h2>YOU'RE CORRECT!</h2> <img src="assets/b2.gif">`);
+        };
+
+      let answerThree;
+        if(userQThreeChoice === 1) {
+          $('.answerThreeResult').html(`<h2>Omg Wrong!!</h2> <img src="assets/shocked.gif">`);
+        } else {
+          $('.answerThreeResult').html(`<h2>YOU'RE CORRECT!</h2> <img src="assets/c1.gif">`);
+        };
+
+      let answerFour;
+        if(userQFourChoice === 1) {
+          $('.answerFourResult').html(`<h2>Omg Wrong!!</h2> <img src="assets/shocked2.gif">`);
+        } else {
+          $('.answerFourResult').html(`<h2>YOU'RE CORRECT!</h2> <img src="assets/milkshake.gif">`);
+        };
+
+      let answerFive;
+        if (userQFiveChoice === 1) {
+          $('.answerFiveResult').html(`<h2>Omg Wrong!!</h2> <img src="assets/shocked.gif">`);
+        } else {
+          $('.answerFiveResult').html(`<h2>YOU'RE CORRECT!</h2> <img src="assets/mall.gif">`);
+        }; 
+
+      let answerSix;
+        if (userQSixChoice === 1) {
+          $('.answerSixResult').html(`<h2>Omg Wrong!!</h2> <img src="assets/shocked2.gif">`);
+        } else {
+          $('.answerSixResult').html(`<h2>YOU'RE CORRECT!</h2> <img src="assets/map.jpg">`);
+        };
+
+      let answerSeven;
+        if (userQSevenChoice === 1) {
+          $('.answerSevenResult').html(`<h2>Omg Wrong!!</h2> <img src="assets/shocked.gif">`);
+        } else {
+          $('.answerSevenResult').html(`<h2>YOU'RE CORRECT!</h2> <img src="assets/seth.gif">`);
+        };
+
+      let answerEight;
+          if(userQEightChoice === 1) {
+            $('.answerEightResult').html(`<h2>Omg Wrong!!</h2> <img src="assets/shocked2.gif">`);
+          } else {
+            $('.answerEightResult').html(`<h2>YOU'RE CORRECT!</h2> <img src="assets/eight.gif">`);
+          };
   }); /* END of form submit! */
 
   $('.clearButton').on('click', function(){
@@ -110,5 +176,15 @@ $(function(){
     $('input[type=radio]').prop('checked', false);
     $('.yourQuote').empty();
     $('.yourImage').empty();
+    $('.answerOneResult').empty();
+    $('.answerTwoResult').empty();
+    $('.answerThreeResult').empty();
+    $('.answerFourResult').empty();
+    $('.answerFiveResult').empty();
+    $('.answerSixResult').empty();
+    $('.answerSevenResult').empty();
+    $('.answerEightResult').empty();
   });
+
+
 }); /*End of document ready*/
